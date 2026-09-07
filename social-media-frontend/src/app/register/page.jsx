db040ba390dd3a3e9da36af7619f62ed99b4f1b7
prativzz <166499719+prativzz@users.noreply.github.com>
2026-09-07 12:55:49 +0530

    Fix register API endpoint

--- social-media-frontend/src/app/register/page.jsx
+++ social-media-frontend/src/app/register/page.jsx
@@ -33,7 +33,7 @@ if (!isValidPassword){
       username:name,
       password:pass
     }
-   const res= await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/login`, {
+   const res= await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/auth/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
